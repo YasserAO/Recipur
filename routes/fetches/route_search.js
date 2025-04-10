d9f1f0bae9deba5bdb09c4270a9d8eda95b4283d
YasserAO <aouffenyasser@gmail.com>
2025-04-10 21:37:37 +0100

    api URL

--- routes/fetches/route_search.js
+++ routes/fetches/route_search.js
@@ -1,44 +1,26 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const router = express.Router();
-const axios = require('axios');
+const axios = require("axios");
 
 const APP_ID = process.env.APP_ID;
 const APP_KEY = process.env.APP_KEY;
 
+router.post("/search", (req, res) => {
+  const query = req.body.searchInputs;
+  if (query !== "") {
+    axios
+      .get(
+        `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+      )
+      .then((ress) => {
+        hit = ress.data.hits;
+        res.send(hit);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }
+});
 
-
-
-
-
-
-
-router.post('/search',(req,res)=>{
-    
-    
-    const query = req.body.searchInputs;
-    if(query!==''){
-        axios.get(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
-        .then(ress=>{
-            hit = ress.data.hits;
-            res.send(hit)
-            
-            
-            
-        
-        }
-            
-            
-        
-    )
-        
-    }
-    
-
-    
-    
-    
-
-})
-
-module.exports=router;
+module.exports = router;
